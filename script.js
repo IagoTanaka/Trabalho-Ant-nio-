6e6c8b1aee24aa2c98170571a4363caa5d68b0dd
IagoTanaka <168189130+IagoTanaka@users.noreply.github.com>
2025-05-30 11:11:11 -0300

    teste1

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+//
+const button = document.getElementById('clickButton');
+const countDisplay = document.getElementById('clickCount');
 
+// Inicializando o contador
+let count = 0;
+
+// Função que será chamada a cada clique
+function handleClick() {
+  count++;  // Incrementa o contador
+  countDisplay.textContent = count;  // Atualiza o número exibido
+}
+
+// Adicionando o evento de clique no botão
+button.addEventListener('click', handleClick);
